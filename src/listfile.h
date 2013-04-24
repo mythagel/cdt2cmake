e0c5d082910593343c0e1747c984b2d10911ad09
mythagel <mythagel@gmail.com>
2013-04-24 16:30:42 +1000

    Work on structure for generating cmake files

--- src/listfile.h
+++ src/listfile.h
@@ -8,7 +8,8 @@
 #ifndef LISTFILE_H_
 #define LISTFILE_H_
 #include <string>
-#include <iosfwd>
+#include <iostream>
+#include <vector>
 
 namespace cmake
 {
@@ -33,8 +34,27 @@ project(${name})
 
 struct comment_t
 {
-	std::string cmt;
+	std::vector<std::string> cmt;
 };
+
+comment_t comment(const std::string& text)
+{
+	comment_t cmt;
+
+	std::istringstream s(text);
+	std::string line;
+	while (std::getline(s, line))
+		cmt.cmt.push_back(line);
+
+	return cmt;
+}
+
+std::ostream& operator<<(std::ostream& os, const comment_t& cmt)
+{
+	for(auto& l : cmt.cmt)
+		os << '#' << l << '\n';
+	return os;
+}
 
 struct variable_t
 {
@@ -47,13 +67,27 @@ struct argument_t
 	std::string arg;
 };
 
+template <typename... Args>
 struct command_t
 {
 	std::string name;
-	// TODO
+	std::tuple<Args...> args;
 };
 
-std::ostream& operator<<(std::ostream& os, const comment_t& cmt);
+template <typename... Args>
+std::ostream& operator<<(std::ostream& os, const command_t<Args...>& cmd)
+{
+	os << cmd.name << " (";
+	// TODO write args
+	os << ")\n";
+	return os;
+}
+
+template <typename... Args>
+command_t<Args...> command(const std::string& name, Args... args)
+{
+	return {name, args...};
+}
 
 }
 
