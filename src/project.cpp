13ad18548c73e5cdf6d50a99f9b27c8d43aa4f93
mythagel <mythagel@gmail.com>
2013-07-11 13:49:24 +1000

    wip

--- src/project.cpp
+++ src/project.cpp
@@ -19,151 +19,6 @@
 namespace cmake
 {
 
-/*
-TODO logic encoded in the following has to be converted to new interface.
-void project_t::clean()
-{
-	{
-		std::set<std::string> includes;
-		for(std::set<std::string>::const_iterator it = artifact.includes.begin(); it != artifact.includes.end(); ++it)
-		{
-			std::string inc = *it;
-			if(inc.empty())
-				continue;
-
-			if(inc.find("\"${workspace_loc:/") == 0)
-			{
-				inc = inc.substr(18);
-				inc = inc.substr(0, inc.size() - 2);
-				inc = "${PROJECT_SOURCE_DIR}/" + inc;
-
-				if(inc[inc.size()-1] != '/')
-					inc += '/';
-
-				includes.insert(inc);
-			}
-			else if(inc.find("../../") == 0)
-			{
-				inc = inc.substr(6);
-				inc = "${PROJECT_SOURCE_DIR}/" + inc;
-
-				if(inc[inc.size()-1] != '/')
-					inc += '/';
-
-				includes.insert(inc);
-			}
-			else
-			{
-				if(inc[inc.size()-1] != '/')
-					inc += '/';
-
-				includes.insert(inc);
-			}
-		}
-		artifact.includes = includes;
-	}
-
-	{
-		std::set<std::string> lib_paths;
-		for(std::set<std::string>::const_iterator it = artifact.lib_paths.begin(); it != artifact.lib_paths.end(); ++it)
-		{
-			std::string lib = *it;
-
-			if(lib.empty())
-				continue;
-			if(lib.find("\"${workspace_loc:/") == 0)
-			{
-				// CMake is smarter than eclipse
-			}
-			else if(lib.find("../../") == 0)
-			{
-				// CMake is smarter than eclipse
-			}
-			else
-			{
-				if(lib[lib.size()-1] != '/')
-					lib += '/';
-
-				lib_paths.insert(lib);
-			}
-		}
-		artifact.lib_paths = lib_paths;
-	}
-
-	{
-		std::vector<std::string> libs;
-		std::set<std::string> seen;
-		for(std::vector<std::string>::const_iterator it = artifact.libs.begin(); it != artifact.libs.end(); ++it)
-		{
-			if(seen.find(*it) == seen.end())
-			{
-				libs.push_back(*it);
-				seen.insert(*it);
-			}
-		}
-		artifact.libs = libs;
-	}
-}
-
-std::ostream& operator<<(std::ostream& os, const project_t& p)
-{
-	os << "cmake_minimum_required (VERSION 2.6)\n";
-	os << "project (" << p.name << ")\n";
-	os << "\n";
-
-	if(!p.artifact.other_flags.empty())
-		os << "set (" << p.name << "_CFLAGS \"" << p.artifact.other_flags << "\")\n\n";
-
-	if(!p.artifact.includes.empty())
-	{
-		os << "set_target_properties (" << p.artifact.name << " INCLUDE_DIRECTORIES\n";
-		for(auto& inc : p.artifact.includes)
-			os << "    " << inc << "\n";
-		os << ")\n";
-		os << "\n";
-	}
-
-	switch(p.artifact.type)
-	{
-		case artifact_t::type_Executable:
-			os << "add_executable (" << p.artifact.name << "\n";
-			break;
-		case artifact_t::type_StaticLibrary:
-			os << "add_library (" << p.artifact.name << " STATIC\n";
-			break;
-		case artifact_t::type_SharedLibrary:
-			os << "add_library (" << p.artifact.name << " SHARED\n";
-			break;
-	}
-	for(auto& src : p.artifact.sources)
-		os << "    " << src << "\n";
-	os << ")\n\n";
-
-	if(!p.artifact.lib_paths.empty())
-	{
-		os << "link_directories (\n";
-		for(auto& path : p.artifact.lib_paths)
-			os << "    " << path << "\n";
-		os << ")\n";
-		os << "\n";
-	}
-
-	if(!p.artifact.libs.empty())
-	{
-		os << "target_link_libraries (" << p.artifact.name << "\n";
-		for(auto& lib : p.artifact.libs)
-			os << "    " << lib << "\n";
-		os << ")\n";
-		os << "\n";
-	}
-
-//	for(auto& dir : p.subdirectories)
-//		os << "add_subdirectory(" << dir << ")\n";
-
-	os << "\n";
-	return os;
-}*/
-
 bool has_c_sources(const std::map<std::string, std::vector<std::string> >& sources)
 {
 	for(const auto& source_folder : sources)
@@ -198,7 +53,7 @@ void merge(const cdt::configuration_t::build_folder::compiler_t& source, cdt::co
 		{
 			inc = inc.substr(18);
 			inc = inc.substr(0, inc.size() - 2);
-			inc = "${PROJECT_SOURCE_DIR}/" + inc;
+			inc = "${CMAKE_SOURCE_DIR}/" + inc;
 
 			if(inc[inc.size()-1] != '/')
 				inc += '/';
@@ -206,7 +61,7 @@ void merge(const cdt::configuration_t::build_folder::compiler_t& source, cdt::co
 		else if(inc.find("../../") == 0)
 		{
 			inc = inc.substr(6);
-			inc = "${PROJECT_SOURCE_DIR}/" + inc;
+			inc = "${CMAKE_SOURCE_DIR}/" + inc;
 
 			if(inc[inc.size()-1] != '/')
 				inc += '/';
@@ -399,7 +254,7 @@ void generate(cdt::project& cdtproject, bool write_files)
 	}
 	std::ostream master(buf);
 	
-	master << "cmake_minimum_required (VERSION 2.6)\n";
+	master << "cmake_minimum_required (VERSION 2.8)\n";
 	master << "project (" << project_name << ")\n";
 	master << "\n";
 
@@ -446,7 +301,8 @@ void generate(cdt::project& cdtproject, bool write_files)
 				{
 					if(!bf.cpp.compiler.includes.empty() || !bf.c.compiler.includes.empty())
 					{
-						master << "set_target_properties (" << c.artifact << " INCLUDE_DIRECTORIES";
+						//master << "set_property (TARGET " << c.artifact << " PROPERTY INCLUDE_DIRECTORIES";
+						master << "INCLUDE_DIRECTORIES(";
 						if(lang_cxx)
 						{
 							for(auto& inc : bf.cpp.compiler.includes)
